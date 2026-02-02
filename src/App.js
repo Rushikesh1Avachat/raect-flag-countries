21e9a745fb5e88dfee32e49110e3905e90d1ab8d
Rushikesh1 Avachat <avachatrushikesh45@gmail.com>
2026-02-02 18:16:55 +0530

    final code for raect falg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import {
+  Container,
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+
+const API_URL = "https://xcountries-backend.labs.crio.do/all";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => setCountries(data))
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="lg" sx={{ marginTop: 4 }}>
+      <Grid container spacing={3}>
+        {countries.map((country) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={country.name}>
+            <Card
+              sx={{
+                height: "100%",
+                textAlign: "center",
+                paddingTop: 2,
+              }}
+            >
+              <CardMedia
+                component="img"
+                image={country.flag}
+                alt={`${country.name} flag`}
+                sx={{
+                  height: 100,
+                  objectFit: "contain",
+                }}
+              />
+              <CardContent>
+                <Typography variant="subtitle1" fontWeight="bold">
+                  {country.name}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 }
 
 export default App;
+
